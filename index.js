84afd366ddbf6140c90d5d0c8bc159a98317859f
MasudRana924 <masud15-924@diu.edu.bd>
2021-11-30 11:45:32 -0600

    bookings done

--- index.js
+++ index.js
@@ -30,6 +30,12 @@ async function run() {
             res.send(hotels)
 
         })
+         app.get('/hotels/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const hotels = await hotelsCollection.findOne(query)
+            res.json(hotels)
+        })
 
         // users section 
         app.post('/users', async (req, res) => {
